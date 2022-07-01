6b0037b67f2d2f472a7e0312872bae4324eaa847
Johan van der Heide <info@jield.nl>
2022-07-01 22:48:40 +0200

    Revert change in entrypoint
    Signed-off-by: Johan van der Heide <info@jield.nl>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,19 +7,19 @@ if [ ! -f /app/thumbor.conf ]; then
   envtpl /app/thumbor.conf.tpl  --allow-missing --keep-template
 fi
 
-## If log level is defined we configure it, else use default log_level = info
-#if [ -n "$LOG_LEVEL" ]; then
-#    LOG_PARAMETER="-l $LOG_LEVEL"
-#fi
+# If log level is defined we configure it, else use default log_level = info
+if [ -n "$LOG_LEVEL" ]; then
+    LOG_PARAMETER="-l $LOG_LEVEL"
+fi
 
-## Check if thumbor port is defined -> (default port 80)
-#if [ -z ${THUMBOR_PORT+x} ]; then
-#    THUMBOR_PORT=80
-#fi
-#
-#if [ "$1" = 'thumbor' ]; then
-#    echo "---> Starting thumbor with ${THUMBOR_NUM_PROCESSES:-1} processes..."
-#    exec thumbor --port=$THUMBOR_PORT --conf=/app/thumbor.conf $LOG_PARAMETER --processes=${THUMBOR_NUM_PROCESSES:-1}
-#fi
+# Check if thumbor port is defined -> (default port 80)
+if [ -z ${THUMBOR_PORT+x} ]; then
+    THUMBOR_PORT=80
+fi
+
+if [ "$1" = 'thumbor' ]; then
+    echo "---> Starting thumbor with ${THUMBOR_NUM_PROCESSES:-1} processes..."
+    exec thumbor --port=$THUMBOR_PORT --conf=/app/thumbor.conf $LOG_PARAMETER --processes=${THUMBOR_NUM_PROCESSES:-1}
+fi
 
 exec "$@"
